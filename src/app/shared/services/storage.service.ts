fb2f12d872b52e1ac2b0d7fa2a31aaa9b121167c
ch-Aleena <aleena.creation246@gmail.com>
2023-09-09 16:05:42 +0500

    change in services folder

--- src/app/shared/services/storage.service.ts
+++ src/app/shared/services/storage.service.ts
@@ -6,14 +6,17 @@ import { Injectable } from '@angular/core';
 export class StorageService {
   constructor() {}
 
+  // Set key,value pair in localStorge
   setitem(key: string, value: string): void {
     localStorage.setItem(key, value);
   }
 
+  //Get value based on key from localStorge
   getitem(key) {
     return localStorage.getItem(key);
   }
 
+  // Clears all the values in localStorage
   clearall(): void {
     localStorage.clear();
   }
